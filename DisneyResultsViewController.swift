31d3c6fb073801f2c984f312431c2fc2d3c23c01
calebakeyes <65526474+calebakeyes@users.noreply.github.com>
2020-05-18 02:20:35 -0400

    Add files via upload

---
+++ DisneyResultsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DisneyResultsViewController.swift
+//  Personality Test
+//
+//  Created by Caleb Keyes on 5/7/20.
+//  Copyright © 2020 Caleb Keyes. All rights reserved.
+//
+
+import UIKit
+
+class DisneyResultsViewController: UIViewController {
+    var responses: [Answer]!
+    func calculatePersonalityResult() {
+        let responseTypes = responses.map { $0.type }
+        var frequencyOfAnswers: [marvelType: Int] = [:]
+        for response in responseTypes {
+           let newCount: Int
+           if let oldCount = frequencyOfAnswers[response] {
+                  newCount = oldCount + 1
+           } else {
+                  newCount = 1
+           }
+           frequencyOfAnswers[response] = newCount
+        }
+        let frequentAnswersSorted = frequencyOfAnswers.sorted(by:
+        { (pair1, pair2) -> Bool in
+                return pair1.value > pair2.value
+        })
+        let mostCommonAnswer = frequentAnswersSorted.first!.key
+        resultAnswerLabel.text = "You are \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        calculatePersonalityResult()
+    }
+    @IBOutlet var resultAnswerLabel: UILabel!
+    @IBOutlet var resultDefinitionLabel: UILabel!
+    
+    @IBAction func restartTest(_ sender: Any) {
+         performSegue(withIdentifier: "unwindToDisneyStart", sender: self)
+}
+}
---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personality Test
+//
+//  Created by Caleb Keyes on 5/7/20.
+//  Copyright © 2020 Caleb Keyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+@IBAction func unwindToMarvelStart(segue:UIStoryboardSegue) { }
+}
+
